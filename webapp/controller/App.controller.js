bc6eb225ade39a4bfe10310ba8730f9ea8ce6608
Aidan Lockwood <aidanlockwood@yahoo.com.au>
2024-09-05 20:16:45 +1000

    Step 6: Modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller, MessageToast) => {
     "use strict";
 
     return Controller.extend("learning.sap.ui5.controller.App", {
         onShowHello() { 
-            alert("Hello World");
+            MessageToast.show("Hello World");
         }
     })
 });
