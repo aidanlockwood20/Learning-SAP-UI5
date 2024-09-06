f6b0ca0898f6d9755ac27d639d1b9d81648a638c
Aidan Lockwood <aidanlockwood@yahoo.com.au>
2024-09-06 10:28:13 +1000

    Step 23: Filtering

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller", 
     'sap/ui/model/json/JSONModel',
-    '../model/formatter'
-], (Controller, JSONModel, formatter) => {
+    '../model/formatter',
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator'
+], (Controller, JSONModel, formatter, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("learning.sap.ui5.controller.InvoiceList", {
@@ -14,8 +16,17 @@ sap.ui.define([
             });
 
             this.getView().setModel(oViewModel, 'view');
-        
+        },
+        onFilterInvoices(oEvent) { 
+            const aFilter = [];
+            const sQuery = oEvent.getParameter('query');
+            if (sQuery) { 
+                aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+            }
 
+            const oList = this.byId('invoiceList');
+            const oBinding = oList.getBinding('items');
+            oBinding.filter(aFilter);
         }
     })
 });
